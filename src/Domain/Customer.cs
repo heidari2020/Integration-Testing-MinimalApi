e4b04f40e3a52e98c3b940608e09ae3074bca744
Kimia Heidari <kimia_heidari1998@outlook.com>
2025-08-25 14:52:54 +0330

    first commit

---
+++ src/Domain/Customer.cs
@@ -0,0 +1,20 @@
+﻿using Domain.ValueObjects;
+using RsjFramework.Entities;
+
+namespace Domain;
+public  class Customer : Entity<long>
+{
+    public FullName FullName { get; protected set; }
+    public string Email { get; protected set; }
+    public DateTime DateOfBirth { get; protected set; }
+    private Customer() { }
+    public Customer(FullName fullName, string email, DateTime dateOfBirth)
+    {
+
+
+        FullName = fullName;
+        Email = email;
+        DateOfBirth = dateOfBirth;
+    }
+     
+}
---
+++ src/Domain/ValueObjects/FullName.cs
@@ -0,0 +1,57 @@
+﻿using RsjFramework.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ValueObjects;
+public class FullName : BaseValueObject<FullName>
+{
+    public string FirstName { get; protected set; }
+    public string LastName { get; protected set; }
+    private FullName(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+    public FullName() { }
+
+    public static Result<FullName> Create(string firstName, string lastName)
+    {
+        if (string.IsNullOrWhiteSpace(firstName))
+            return Result.Fail<FullName>("Value is required for FirstName.");
+        if (string.IsNullOrWhiteSpace(lastName))
+            return Result.Fail<FullName>("Value is required for LastName.");
+        if (firstName.Length > 100)
+        {
+            return Result.Fail<FullName>("The FirstName length is a maximum of 100 characters.");
+
+        }
+        if (lastName.Length > 100)
+        {
+            return Result.Fail<FullName>("The LastName length is a maximum of 100 characters.");
+        }
+        return Result.Ok(new FullName(firstName, lastName));
+    }
+
+    protected override int GetHashCodeCore()
+    {
+        return FirstName.GetHashCode() + LastName.GetHashCode();
+    }
+
+    protected override bool IsEqual(FullName other)
+    {
+        return FirstName.Equals(other.FirstName, StringComparison.InvariantCultureIgnoreCase) && LastName.Equals(other.LastName, StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    public static implicit operator FullName(Result<FullName> value)
+    {
+        return value.Value;
+    }
+
+    public override string ToString()
+    {
+        return $"{FirstName ?? ""} {LastName ?? ""}";
+    }
+}
