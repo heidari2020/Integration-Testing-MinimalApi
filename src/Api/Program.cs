fc2187621c3192340f5e23fb7f02f5dc671cbbb6
Kimia Heidari <kimia_heidari1998@outlook.com>
2026-07-31 22:49:51 +0330

    +EditDockerize

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Application;
 using Infrastructure;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
@@ -10,8 +12,8 @@ using Presentation.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddApplication();  
-builder.Services.AddInfrastructure(builder.Configuration); 
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -20,8 +22,14 @@ builder.Services.AddSwaggerGen(c =>
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Project Management API", Version = "v1" });
 });
 
+
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate();
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
